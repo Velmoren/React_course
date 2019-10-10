facd8e6ed3e7f3f50b2735b6394a9b5ba6062de0
Velmoren <velmoren@yahoo.com>
2019-10-11 00:10:43 +0300

    lesson_03

---
+++ my-app/src/components/post-add-form/post-add-form.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const PostAddForm = () => {
+    return (
+        <form className="botoom-panel d-flex">
+            <input
+                type="text"
+                placeholder="О чем вы думаете сейчас"
+                className="form-control new-post-label"
+            />
+            <button
+                type="submit"
+                className="btn btn-outline-secondary">
+                Добавить
+            </button>
+        </form>
+    )
+}
+
+export default PostAddForm;
