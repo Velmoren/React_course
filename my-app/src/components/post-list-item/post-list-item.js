93327c9ff6718177259ef3e132d0f9252e71fb48
Velmoren <velmoren@yahoo.com>
2019-10-11 05:23:54 +0300

    правки

--- my-app/src/components/post-list-item/post-list-item.js
+++ my-app/src/components/post-list-item/post-list-item.js
@@ -1,24 +1,28 @@
 import React from "react";
 
+import "./post-list-item.css";
+
 const PostListItem = () => {
     const date = new Date().toLocaleString("ru");
 
     return (
         <li className="app-list-item d-flex justify-content-between">
             <span className="app-list-item-label">Hello World!</span>
-            <span className="post-list-item-date">{date}</span>
-            <div className="d-flex justify-content-center align-items-center"> 
-                <button 
-                    type="button" 
-                    className="btn-star btn-sm">
-                    <i className="fa fa-star"></i>
-                </button>
-                <button 
-                    type="button" 
-                    className="btn-trash btn-sm">
-                    <i className="fa fa-trash-o"></i>
-                </button>
-                <i className="fa fa-heart"></i>
+            <div className="d-flex flex-column">
+                <div className="d-flex justify-content-center align-items-center"> 
+                    <button 
+                        type="button" 
+                        className="btn-star btn-sm">
+                        <i className="fa fa-star"></i>
+                    </button>
+                    <button 
+                        type="button" 
+                        className="btn-trash btn-sm">
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                    <i className="fa fa-heart"></i>
+                </div>
+                <span className="post-list-item-date align-self-start">{date}</span>
             </div>
         </li>
     )
