dee87c93d0af4e2bc4f83d46a9e23732aeb08cf2
Velmoren <velmoren@yahoo.com>
2019-11-06 09:58:20 +0300

    v1.0

---
+++ rest/src/components/app/app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MainPage, CartPage } from '../pages';
+import AppHeader from '../app-header';
+import WithRestoService from '../hoc'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Background from './food-bg.jpg';
+
+const App = ({ RestoService }) => {
+
+    console.log(RestoService.getMenuItems(`http://localhost:3000/menu`));
+
+    return (
+        <Router>
+            <div style={{ background: `url(${Background}) center center/cover no-repeat` }} className="app">
+                <AppHeader total={50} />
+                <Route path='/' exact component={MainPage} />
+                <Route path='/cart' component={CartPage} />
+                {/* <MainPage />
+                <CartPage /> */}
+            </div>
+        </Router>
+    )
+}
+
+export default WithRestoService()(App);
---
+++ rest/src/services/resto-service.js
@@ -0,0 +1,11 @@
+export default class RestoService {
+    getMenuItems = async (url) => {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+
+        return await res.json();
+    }
+}
